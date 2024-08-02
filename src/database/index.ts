339fef76ad0a8c74294a0b0c0937bd18bdc5e3e1
TheTesterss <morganjaouen65636@gmail.com>
2024-08-03 00:18:31 +0200

    Delete src directory

--- src/database/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './ClientDB';
-export * from './GuildDB';
-export * from './MemberDB';
-export * from './UserDB';
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-import 'colors';
-import Bot from './classes/Bot';
-
-new Bot().startClient(process.env.token);
