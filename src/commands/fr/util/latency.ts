2d71d8084c17717acebf9d655b302f28d60c7982
TheTesterss <morganjaouen65636@gmail.com>
2024-08-22 15:53:45 +0200

    Update latency.ts

--- src/commands/fr/util/latency.ts
+++ src/commands/fr/util/latency.ts
@@ -6,20 +6,20 @@ import {
     EmbedBuilder,
     MessageContextMenuCommandInteraction,
     UserContextMenuCommandInteraction
-} from "discord.js";
-import { CommandDatas } from "../../../enums/Interfaces";
-import Bot from "../../../classes/Bot";
-import Database from "../../../classes/Database";
-import { LangValues } from "../../../enums/enums";
+} from 'discord.js';
+import { CommandDatas } from '../../../enums/Interfaces';
+import Bot from '../../../classes/Bot';
+import Database from '../../../classes/Database';
+import { LangValues } from '../../../enums/enums';
 
 export const command: CommandDatas = {
-    name: "latencies",
+    name: 'latencies',
     nameLocalizations: {
-        fr: "latences"
+        fr: 'latences'
     },
-    description: "Show the differents client latencies.",
+    description: 'Show the differents client latencies.',
     descriptionLocalizations: {
-        fr: "Affiche les différentes latences du client."
+        fr: 'Affiche les différentes latences du client.'
     },
     options: [],
     customOptions: {
@@ -33,7 +33,7 @@ export const command: CommandDatas = {
         isNSFW: false,
         whitelistDisallowed: false,
         memberRequiredPermissions: [],
-        clientRequiredPermissions: []
+        clientRequiredPermissions: undefined
     },
     types: [ApplicationCommandType.ChatInput],
     execute: async (
@@ -48,17 +48,17 @@ export const command: CommandDatas = {
     ): Promise<void> => {
         let embed = new EmbedBuilder().setColor(bot.colors.true as ColorResolvable).setFooter({
             iconURL: interaction.client.user.avatarURL() ?? undefined,
-            text: "Alimenté par Aunt Développement"
+            text: 'Alimenté par Aunt Développement'
         });
 
         let message = await interaction.editReply({
-            embeds: [embed.setDescription(`${bot.customEmojis.chat} - Recherche des latences...`)]
+            embeds: [embed.setDescription(`<:1422navoteicon:1271775782426902598> - Recherche des latences...`)]
         });
         await message.edit({
             embeds: [
-                embed.setDescription(`${bot.customEmojis.chat} - Latences trouvées.`).addFields({
+                embed.setDescription(`<:1422navoteicon:1271775782426902598> - Latences trouvées.`).addFields({
                     name: `<t:${Math.round(Date.now() / 1000)}:R>`,
-                    value: `${bot.customEmojis.stats} - **MS Passerelle** - \`${bot.djsClient!.ws.ping}ms\`\n${bot.customEmojis.stats} - **MS Réponse** - \`${Math.round(message.createdTimestamp / interaction.createdTimestamp)}ms\`\n${bot.customEmojis.stats} - **Connecté** - <t:${Math.round(bot.djsClient!.readyTimestamp! / 1000)}:R>`,
+                    value: `<:8614naboosticon:1271775921166221312> - **MS Passerelle** - \`${bot.djsClient!.ws.ping}ms\`\n<:6123nacompassicon:1271775852434034688> - **MS Réponse** - \`${Math.round(message.createdTimestamp / interaction.createdTimestamp)}ms\`\n<:7506namodcommunityicon:1271775882595536926> - **Connecté** - <t:${Math.round(bot.djsClient!.readyTimestamp! / 1000)}:R>`,
                     inline: true
                 })
             ]
